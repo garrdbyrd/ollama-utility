284bd74004952be769a8ae0e7536ea6de7c8bb06
Garrett Byrd <garrdbyrd@gmail.com>
2024-03-24 12:38:35 -0400

    Added PORT variable

--- run-ollama.sh
+++ run-ollama.sh
@@ -15,6 +15,7 @@ error_handler() {
 SHORT="m:d:h"
 LONG="model:,directory:,help"
 PARSED=$(getopt --options $SHORT --longoptions $LONG --name "$0" -- "$@")
+PORT=11434
 
 if [[ $? -ne 0 ]]; then
     exit 2
@@ -62,11 +63,11 @@ echo "Model:     $model"
 echo "Directory: $directory"
 
 sudo systemctl start docker
-docker run -d --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p 11434:11434 --name ollama ollama/ollama:rocm
+docker run -d --device /dev/kfd --device /dev/dri -v ollama:/root/.ollama -p ${PORT}:${PORT} --name ollama ollama/ollama:rocm
 sudo docker exec -d ollama ollama run $model 
 
 prompt="Why is the sky blue?"
-response=$(curl -s -X POST http://localhost:11434/api/generate -d '{
+response=$(curl -s -X POST http://localhost:${PORT}/api/generate -d '{
   "model": "'"$model"'",
   "prompt": "'"$prompt"'",
   "stream": false
